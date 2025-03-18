cd91c0fa85ccd3ca8cef616677b3c26678db78fd
diegowh <contacto.diegowh@gmail.com>
2025-03-18 16:23:42 +0100

    Refactor service

--- app/src/main/java/com/example/konohaproject/controller/CountdownService.kt
+++ app/src/main/java/com/example/konohaproject/controller/CountdownService.kt
@@ -1,198 +1,84 @@
 package com.example.konohaproject.controller
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
-import android.os.Build
 import android.os.IBinder
-import android.os.SystemClock
-import android.util.Log
-import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-class CountdownService : Service() {
+class CountdownService : Service(), CountdownController, CountdownTimer.Listener {
+
+
+    private val binder = CountdownBinder()
+    private lateinit var notificationHelper: NotificationHelper
+    private lateinit var countdownTimer: CountdownTimer
+    private var timeListener: TimeUpdateListener? = null
+
+    private val serviceScope = CoroutineScope(Dispatchers.Default)
+
+    inner class CountdownBinder: Binder() {
+        fun getController(): CountdownController = this@CountdownService
+    }
+
+
 
     interface TimeUpdateListener {
         fun onTimeUpdate(remainingTime: Long)
     }
 
-    var timeListener: TimeUpdateListener? = null
-
-    private val serviceScope = CoroutineScope(Dispatchers.Default)
-    private var countDownJob: Job? = null
-    private var endTime: Long = 0L
-
-    inner class CountdownBinder : Binder() {
-        fun getService(): CountdownService = this@CountdownService
-    }
-
-    companion object {
-        @Volatile
-        private var isRunning: Boolean = false
-
-        @Volatile
-        private var isPaused: Boolean = false
-
-        @Volatile
-        private var remainingTime: Long = 0L
-
-        fun isCountDownActive() = isRunning && !isPaused
-        fun isPaused() = isPaused
-
-        internal fun setRunning(running: Boolean) {
-            isRunning = running
-            if (!running) {
-                isPaused = false
-                remainingTime = 0L
-            }
-        }
-
-        internal fun setPaused(paused: Boolean, time: Long = 0L) {
-            isPaused = paused
-            if (paused) remainingTime = time
-        }
-
-        const val TAG = "CountdownService"
-        const val CHANNEL_ID = "countdown_channel"
-        const val NOTIFICATION_ID = 101
-    }
-
-    override fun onBind(intent: Intent?): IBinder? = CountdownBinder()
-
     override fun onCreate() {
         super.onCreate()
-        createNotificationChannel()
-        startForeground(NOTIFICATION_ID, createNotification())
+        notificationHelper = NotificationHelper(this)
+        countdownTimer = CountdownTimer(serviceScope, this)
+        startForegroundService()
     }
 
-    private fun createNotification(): Notification {
-        return NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Contador en ejecución")
-            .setContentText("El contador está activo en segundo plano")
-            .setSmallIcon(android.R.drawable.ic_dialog_info)
-            .setPriority(NotificationCompat.PRIORITY_LOW)
-            .setOngoing(true)  // Notificación persistente
-            .build()
+    private fun startForegroundService() {
+        startForeground(
+            NotificationHelper.NOTIFICATION_ID,
+            notificationHelper.buildDefaultNotification()
+        )
     }
+    override fun onBind(intent: Intent?): IBinder? = binder
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if (intent?.action == "STOP") {
-            stopSelf()
-            return START_NOT_STICKY
-        }
-
-        if (!isRunning) {
-            setRunning(true)
-            val duration = intent?.getLongExtra("duration", 0L) ?: 0L
-            startCountdown(duration)
-
+        when (intent?.action) {
+            ACTION_STOP -> stopSelf()
+            else -> intent?.getLongExtra(EXTRA_DURATION, 0L)?.let {
+                if (!countdownTimer.isRunning()) countdownTimer.start(it)
+            }
         }
         return START_STICKY
     }
 
-    private fun startCountdown(durationMillis: Long) {
-        countDownJob?.cancel();
-        endTime = SystemClock.elapsedRealtime() + durationMillis
-
-        countDownJob = serviceScope.launch {
-            setPaused(false)
-            while (SystemClock.elapsedRealtime() < endTime) {
-                if (isPaused){
-                    setPaused(true, endTime - SystemClock.elapsedRealtime())
-                    delay(100)
-                    continue
-                }
-                logRemainingTime(endTime - SystemClock.elapsedRealtime())
-                delay(1000)
-
-            }
-            if (!isPaused) {
-                stopSelf()
-            }
-        }
-    }
-
-    fun pauseCountdown() {
-        if (isRunning && !isPaused) {
-            setPaused(true, endTime - SystemClock.elapsedRealtime())
-            countDownJob?.cancel()
-        }
-    }
-
-    fun resumeCountdown() {
-        if (isRunning && isPaused) {
-            setPaused(false)
-            endTime = SystemClock.elapsedRealtime() + remainingTime
-            startCountdown(remainingTime)
-        }
-    }
-
-    private fun logRemainingTime(remaining: Long) {
-        val minutes = remaining / 1000 / 60
-        val seconds = remaining / 1000 % 60
-        timeListener?.onTimeUpdate(remaining)
-    }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                CHANNEL_ID,
-                "Contador regresivo",
-                NotificationManager.IMPORTANCE_LOW
-            )
-            getSystemService(NotificationManager::class.java)?.createNotificationChannel(channel);
-        }
-    }
-
-    private fun startForegroundService() {
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Contador en ejecucion")
-            .setContentText("El contador esta activo en segundo plano")
-            .setSmallIcon(android.R.drawable.ic_dialog_info)
-            .build()
-
-        startForeground(NOTIFICATION_ID, notification)
-    }
-
     override fun onDestroy() {
+        countdownTimer.reset()
         super.onDestroy()
-        setRunning(false)
-        countDownJob?.cancel()
-        endTime = 0L
-        remainingTime = 0L
-        stopForeground(STOP_FOREGROUND_REMOVE)
+    }
+
+    override fun onTimeUpdate(remainingTime: Long) {
+        timeListener?.onTimeUpdate(remainingTime)
+        notificationHelper.updateNotification(this, remainingTime)
+    }
+
+    override fun onCountdownFinished() {
         stopSelf()
     }
 
-    public fun isPaused(): Boolean {
-        return isPaused
+    override fun start(durationMillis: Long) = countdownTimer.start(durationMillis)
+    override fun pause() = countdownTimer.pause()
+    override fun resume() = countdownTimer.resume()
+    override fun reset() = countdownTimer.reset()
+    override fun getRemainingTime(): Long = countdownTimer.getRemainingTime()
+    override fun isPaused(): Boolean = countdownTimer.isPaused()
+    override fun isRunning(): Boolean = countdownTimer.isRunning()
+    override fun setTimeUpdateListener(listener: TimeUpdateListener?) {
+        timeListener = listener
     }
 
-    fun fullReset() {
-        hardReset()
-        remainingTime = 25 * 60 * 1000L
-    }
-
-    fun hardReset() {
-        countDownJob?.cancel()
-        endTime = 0L
-        remainingTime = 0L
-        setRunning(false)
-        setPaused(false)
-    }
-
-    fun getCurrentRemainingTime(): Long {
-        return if (isRunning) {
-            if (isPaused) remainingTime else endTime - SystemClock.elapsedRealtime()
-        } else {
-            0L
-        }
+    companion object {
+        const val ACTION_STOP = "STOP"
+        const val EXTRA_DURATION = "duration"
     }
 }
