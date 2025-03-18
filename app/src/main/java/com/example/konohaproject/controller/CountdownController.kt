672f453a02e0e841ff9cc1989629323d0fdaf776
diegowh <contacto.diegowh@gmail.com>
2025-03-18 16:23:16 +0100

    Create CountdownController.kt

---
+++ app/src/main/java/com/example/konohaproject/controller/CountdownController.kt
@@ -0,0 +1,14 @@
+package com.example.konohaproject.controller
+
+import com.example.konohaproject.controller.CountdownService.TimeUpdateListener
+
+interface CountdownController {
+    fun start(durationMillis: Long)
+    fun pause()
+    fun resume()
+    fun reset()
+    fun getRemainingTime(): Long
+    fun isPaused(): Boolean
+    fun isRunning(): Boolean
+    fun setTimeUpdateListener(listener: TimeUpdateListener?)
+}
