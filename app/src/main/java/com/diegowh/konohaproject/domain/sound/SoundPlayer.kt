55a4aeecbbe0a17dd7d218eedd72a21ed034f907
diegowh <contacto.diegowh@gmail.com>
2025-04-18 14:23:44 +0200

    Create SoundPlayer.kt

---
+++ app/src/main/java/com/diegowh/konohaproject/domain/sound/SoundPlayer.kt
@@ -0,0 +1,27 @@
+package com.diegowh.konohaproject.domain.sound
+
+import android.content.Context
+import android.media.AudioManager
+import android.media.SoundPool
+import com.diegowh.konohaproject.utils.SoundType
+
+class SoundPlayer(private val context: Context) {
+
+    private val soundPool = SoundPool.Builder().setMaxStreams(2).build()
+    private val sounds = mutableMapOf<SoundType, Int>()
+
+    fun loadSound(type: SoundType, resId: Int) {
+        sounds[type] = soundPool.load(context, resId, 1)
+    }
+
+    fun play(type: SoundType) {
+        sounds[type]?.let { id ->
+            val vol = (context.getSystemService(Context.AUDIO_SERVICE) as AudioManager)
+                .getStreamVolume(AudioManager.STREAM_MUSIC).toFloat()
+            soundPool.play(id, vol, vol, 1, 0, 1.0f)
+        }
+    }
+
+    fun release() = soundPool.release()
+
+}
