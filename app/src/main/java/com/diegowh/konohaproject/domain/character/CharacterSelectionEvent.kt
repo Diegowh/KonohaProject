fd63a262eb3d7891fa6a281482f51b8d939208f7
diegowh <contacto.diegowh@gmail.com>
2025-05-05 18:38:39 +0200

    Delete CharacterSelectionEvent.kt

--- app/src/main/java/com/diegowh/konohaproject/domain/character/CharacterSelectionEvent.kt
+++
@@ -1,5 +0,0 @@
-package com.diegowh.konohaproject.domain.character
-
-sealed class CharacterSelectionEvent {
-    data class CharacterSelected(val character: Character) : CharacterSelectionEvent()
-}
