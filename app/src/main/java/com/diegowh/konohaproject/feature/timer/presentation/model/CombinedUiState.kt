ea5e21671692601b37ac852053c451997c61e99a
diegowh <contacto.diegowh@gmail.com>
2025-05-13 18:57:27 +0200

    Delete CombinedUiState.kt

--- app/src/main/java/com/diegowh/konohaproject/feature/timer/presentation/model/CombinedUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.diegowh.konohaproject.feature.timer.presentation.model
-
-data class CombinedUiState(
-    val timer: TimerUiState,
-    val character: CharacterUiState,
-    val animation: AnimationUiState,
-    val settings: SettingsUiState,
-    val screen: ScreenUiState
-)
