97957265d9d1415791ee505c57242e43039d2a00
diegowh <contacto.diegowh@gmail.com>
2025-05-17 20:19:27 +0200

    Create ShopManager.kt

---
+++ app/src/main/java/com/diegowh/konohaproject/feature/shop/ShopManager.kt
@@ -0,0 +1,12 @@
+package com.diegowh.konohaproject.feature.shop
+
+import com.diegowh.konohaproject.feature.xp.domain.CoinManager
+import com.diegowh.konohaproject.feature.xp.domain.XpRepository
+
+class ShopManager(
+    private val xpRepository: XpRepository,
+    private val coinManager: CoinManager
+) {
+
+
+}
